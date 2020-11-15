16db0c7febe3975d2d176a38607d335ce4fb5f7a
kipariss1 <kpassudin@gmail.com>
2020-11-15 20:16:35 +0100

    Change decodes raw GPS data

--- first_success.ino
+++ first_success.ino
@@ -1,12 +1,22 @@
-#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+
+int SerialBaud = 9600;
+
+TinyGPSPlus gps;          // TinyGPS++ object
 
 void setup() {
-  Serial.begin(9600);         // Setting the Bode rate for the 0(RX), 1(TX) ports
-  Serial1.begin(9600);        // Setting the Bode rate for the 19(RX), 18(TX) ports
+  Serial.begin(SerialBaud);         // Setting the Bode rate for the 0(RX), 1(TX) ports
+  Serial1.begin(SerialBaud);        // Setting the Bode rate for the 19(RX), 18(TX) ports
 }
 
 void loop() {
-  if (Serial1.available()) {              // if something is happening on the 19, 18 pins
-    Serial.write(Serial1.read());         // read 19, 18 pin and write it on the 0, 1 pins
+  if (Serial1.available()){                       // Checking if there is something on 19, 18 pins 
+    gps.encode(Serial1.read());                   // Encoding raw gps data with gps object 
+    if (gps.location.isUpdated()){                // Checking if location is updated 
+      Serial.print("Latitude = ");                
+      Serial.println(gps.location.lat(), 6);      // Printing the lattitude 
+      Serial.print("Longtitude = ");
+      Serial.println(gps.location.lng(), 6);      // Printing the longtitude 
+    }
   } 
 }
