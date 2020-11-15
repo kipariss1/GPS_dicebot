55b28c92b289646bf8c6d7dedbdd33a440e4d220
kipariss1 <kpassudin@gmail.com>
2020-11-15 19:01:45 +0100

    Comments added

--- first_success.ino
+++ first_success.ino
@@ -1,14 +1,12 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial ss(19, 18);
-
 void setup() {
-  Serial.begin(9600);
-  Serial1.begin(9600);
+  Serial.begin(9600);         // Setting the Bode rate for the 0(RX), 1(TX) ports
+  Serial1.begin(9600);        // Setting the Bode rate for the 19(RX), 18(TX) ports
 }
 
 void loop() {
-  if (Serial1.available()) {
-    Serial.write(Serial1.read());
+  if (Serial1.available()) {              // if something is happening on the 19, 18 pins
+    Serial.write(Serial1.read());         // read 19, 18 pin and write it on the 0, 1 pins
   } 
 }
